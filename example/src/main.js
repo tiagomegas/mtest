ad0554e5fa4935f3b630581ef8fd45de258cf862
megas <tiago.magalhaes@moontrip.pt>
2016-11-16 00:15:54 +0000

    new build

--- example/src/main.js
+++ example/src/main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import Router from './router'
 
-//const store = createStore(reducer);
 let rootElement = document.getElementById('musikkiTest');
 
 render(
